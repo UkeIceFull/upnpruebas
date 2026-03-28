8b72c918eef667a3e4b3a7eae652b0aae13d1b59
UkeIceFull <victorfernandezpoma@gmail.com>
2026-03-28 09:06:54 -0500

    Initial commit

---
+++ test1.py
@@ -0,0 +1,3 @@
+nombre = input("Ingrese su nombre \n")
+edad = int(input("Ingrese su edad \n"))
+print("Hola", nombre, "tu edad el proximo año sera", edad + 1)
---
+++ test2.py
@@ -0,0 +1,4 @@
+n1=int(input("Ingrese el primer num" "\n")); 
+n2=int(input("Ingrese un segundo" "\n")); 
+suma=(n1 + n2);
+print("Resultado:", suma); 
